dcfb6a6d7f695b9907808747d0b24cc156b84b19
guidolarosa <guido.glarosa@gmail.com>
2023-02-15 23:12:07 +0100

    Updated education

--- src/data/education.js
+++ src/data/education.js
@@ -3,6 +3,7 @@ const education = [
   'After that I got accepted in ENERC, which is the National Cinema School. I learned a lot about the industry and Sound Design for Cinema, which was my specialization, but found out that I was not very interested in this kind of career.',
   'Around that time I discovered code. A friend introduced me to Frontend Development and I started an intense self-learning path diving deep into web technologies. I learned about the basics (HTML, CSS, JS) and then I started working on some real world projects, which taught me a lot in a short period of time.',
   'Meanwhile I learned a lot of stuff related to computer graphics, illustration, animation, design and design tools. I also worked briefly doing Motion Graphics and making illustrations (you can see them following my links).',
+  'Now, I consider myself a React developer. I\'ve been working with react for the past 5 years and I love it. It makes creating interfaces a delight and the way it lets you compose reusable components makes it a great choice when working closely with designers.'
 ];
 
 module.exports = education;
